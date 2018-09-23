ff7fb88c10e9ab14059ba3d1fc4253706cf72733
ZwerOxotnik <zweroxotnik@gmail.com>
2018-09-23 13:00:27 +0300

    Change buttons

--- secondary-chat/gui/buttons.lua
+++ secondary-chat/gui/buttons.lua
@@ -1,30 +1,16 @@
-function toggle_chat_part(input_name)
+function toggle_chat_part(name)
   local player = game.player
   local flow = player.gui.left.table_chat.buttons
-  local name_button = 'chat_' .. input_name
-  local button = flow[name_button]
-  if button then
-    button.destroy()
+  local table_name = 'table_' .. name
+  local main_table = flow[table_name]
+  if main_table then
+    main_table.destroy()
   else
-    local button = flow.add{type = 'button', name = name_button, caption = {'', {'secondary_chat.send_to'}, ' ', {'secondary_chat_list.' .. input_name}}}
+    local main_table = flow.add{type = 'table', name = table_name, column_count = 1}
+    local table = main_table.add{type = 'table', name = 'main', column_count = 8}
+    local button = table.add{type = 'button', name = 'chat_' .. name, caption = {'', {'secondary_chat.send_to'}, ' ', {'secondary_chat_list.' .. name}}}
     button.style.font = 'default'
     button.style.top_padding = 0
     button.style.bottom_padding = 0
-  end
-end
-
-function sc_change_button_allied(player)
-  local table_chat = player.gui.left.table_chat
-  if table_chat ~= nil then
-    if is_force_have_allies(player.force) then
-      if table_chat.chat_allied == nil then
-        local button = table_chat.add{type = 'button', name = 'chat_allied', caption = {'gui.chat_allied'}}
-        button.style.font = 'default'
-        button.style.top_padding = 0
-        button.style.bottom_padding = 0
-      end
-    elseif table_chat.chat_allied ~= nil then
-      table_chat.chat_allied.destroy()
-    end
   end
 end
