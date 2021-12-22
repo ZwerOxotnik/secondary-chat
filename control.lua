e52eed8b264862a5e922ec95c89d66320426314a
ZwerOxotnik <zweroxotnik@gmail.com>
2021-12-22 19:57:48 +0300

    Update to 1.25.1

--- control.lua
+++ control.lua
@@ -1,7 +1,7 @@
 if script.level.campaign_name then return end
 
-event_listener = require("event_handler")
+local event_handler = require("__zk-lib__/static-libs/lualibs/event_handler_vZO.lua")
 local modules = {}
 modules.secondary_chat = require("secondary-chat/control")
 
-event_listener.add_libraries(modules)
+event_handler.add_libraries(modules)
